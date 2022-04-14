458383fac563bc3eae31b31f0df120398fc33cdb
LevSarge <levimsargeant@gmail.com>
2022-04-14 17:37:52 -0600

    Finished part 1

--- weather.py
+++ weather.py
@@ -1,14 +1,24 @@
 import requests
 
-api_key = "2499e65f4a958ff5b07f90449e9efaeb"
-city = "Orlando"
-url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key+""
+def get_weather_desc_and_temp():
+    api_key = "2499e65f4a958ff5b07f90449e9efaeb"
+    city = "Orlando"
+    url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key+""
 
-request = requests.get(url)
-json = request.json()
-print(json)
+    request = requests.get(url)
+    json = request.json()
+    print(json)
 
-description = json.get("weather")[0].get("description")
-print("Today's forecast is", description)
-temp_min = json.get("main").get("temp_min")
-print("The minimum temperature is", temp_min)
+    description = json.get("weather")[0].get("description")
+    temp_min = json.get("main").get("temp_min")
+    temp_max = json.get("main").get("temp_max")
+
+    return {'description': description,
+            'temp_min':temp_min,
+            'temp_max':temp_max}
+
+def main():
+    weather_dict = get_weather_desc_and_temp()
+    print("Today's forecast is", weather_dict.get('description'))
+    print("The minimum temperature is", weather_dict.get('temp_min'))
+    print("The maximum temperature is", weather_dict.get('temp_max'))
